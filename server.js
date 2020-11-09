3afa936cdcc2f75d702c128282c267b9adc4ae47
mrcau <mrcau98@gmail.com>
2020-11-10 06:59:49 +0900

    second2

--- server.js
+++ server.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(8080,()=>{
   console.log('hi8080');
 });
 
-app.get('/pet',(req,res) => {
-  res.send('하이페이지');
-})
 
 app.get('/babo',(req,res) => {
   res.send('바보바보')
 })
 
-app.get('/chun',(req,res) => {
-  res.send('천재천재')
-})
 
-app.get('/gut',(req,res) => {
-  res.send('굿~! 굿~!')
-})
-
-app.get('/haha',(req,res) => {
-  res.send('하하하하하하하핳하')
+app.get('/write',(req,res) => {
+  res.sendFile(__dirname+'/write.html')
 })
 
 
 app.get('/',(req,res) => {
   res.sendFile(__dirname+'/index.html');})
+
+  app.post('/add', function(req, res){
+    console.log(req.body);
+    res.send('전송완료')
+  });
